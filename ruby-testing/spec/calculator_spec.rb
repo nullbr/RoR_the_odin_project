3067c0f689e494b23c304fe9a12d8cabba49152c
nullbr <bmarianoleite3@gmail.com>
2022-06-08 20:00:59 -0300

    chore: break code and refactor

--- ruby-testing/spec/calculator_spec.rb
+++ ruby-testing/spec/calculator_spec.rb
@@ -8,5 +8,10 @@ describe Calculator do
       calculator = Calculator.new
       expect(calculator.add(5, 2)).to eql(7)
     end
+
+    it 'returns the sum of more than two numbers' do
+      calculator = Calculator.new
+      expect(calculator.add(5, 2, 6)).to eql(13)
+    end
   end
 end
