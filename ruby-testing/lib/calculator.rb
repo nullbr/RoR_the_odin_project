032ff024831960ff88859693ec8df6c503fd3815
nullbr <bmarianoleite3@gmail.com>
2022-06-08 20:16:23 -0300

    chore: testing multiply and divide methods

--- ruby-testing/lib/calculator.rb
+++ ruby-testing/lib/calculator.rb
@@ -2,4 +2,16 @@ class Calculator
   def add(a, b, c = 0)
     a + b + c
   end
+
+  def subtract(a, b, c = 0)
+    a - b - c
+  end
+
+  def multiply(a, b, c = 1)
+    a * b * c
+  end
+
+  def divide(a, b, c = 1)
+    a / b / c unless b.zero? || c.zero?
+  end
 end
--- ruby-testing/spec/calculator_spec.rb
+++ ruby-testing/spec/calculator_spec.rb
@@ -14,4 +14,50 @@ describe Calculator do
       expect(calculator.add(5, 2, 6)).to eql(13)
     end
   end
+
+  describe '#subtract' do
+    it 'returns the subtraction of two numbers' do
+      calculator = Calculator.new
+      expect(calculator.subtract(5, 2)).to eql(3)
+    end
+
+    it 'returns the subtraction of more than two numbers' do
+      calculator = Calculator.new
+      expect(calculator.subtract(15, 2, 5)).to eql(8)
+    end
+  end
+
+  describe '#multiply' do
+    it 'returns the multiplication of two numbers' do
+      calculator = Calculator.new
+      expect(calculator.multiply(5, 6)).to eql(30)
+    end
+
+    it 'returns the multiplication of more than two numbers' do
+      calculator = Calculator.new
+      expect(calculator.multiply(5, 6, 10)).to eql(300)
+    end
+  end
+
+  describe '#divide' do
+    it 'returns the division of two numbers' do
+      calculator = Calculator.new
+      expect(calculator.divide(10, 2)).to eql(5)
+    end
+
+    it 'returns the division of more than two numbers' do
+      calculator = Calculator.new
+      expect(calculator.divide(10, 2, 1)).to eql(5)
+    end
+
+    it 'returns nil if division by 0' do
+      calculator = Calculator.new
+      expect(calculator.divide(10, 0)).to eql(nil)
+    end
+
+    it 'returns nil if division by 0 with a third number' do
+      calculator = Calculator.new
+      expect(calculator.divide(10, 1, 0)).to eql(nil)
+    end
+  end
 end
